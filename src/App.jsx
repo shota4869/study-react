1d1bc7a37808a1859d8f51625ebf42a80edf7863
shota4869 <81454257+shota4869@users.noreply.github.com>
2022-02-27 12:55:15 +0900

    再レンダリングの学習

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off*/
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初"); //State更新時にコンポーネントを再レンダリングされるので上から処理が走る
+  const [num, setNum] = useState(0); //State
+  const [faceShowFlg, setFaceShowFlg] = useState(false);
+
   const onClickButton = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0); //State
+
+  const onClickSwichShowFlg = () => {
+    setFaceShowFlg(!faceShowFlg);
+  };
+
+  useEffect(() => {
+    console.log("useeffect");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlg || setFaceShowFlg(true);
+      } else {
+        faceShowFlg && setFaceShowFlg(false);
+      }
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]); //最初の一回だけ処理を走らせたいときだけ
+  //numに関心を向けるのでnumが変更されたとき以外はこの処理が走らない
 
   return (
     <>
@@ -13,7 +34,9 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickButton}>カウントアップ</button>
+      <button onClick={onClickSwichShowFlg}>on/off</button>
       <p>{num}</p>
+      {faceShowFlg && <p>(~~)</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  console.log("子"); //親のコンポーネントが変更されたら再レンダリングされる
   const { color, children } = props; //分割代入
   const contentStyle = {
     color, //プロパティ名と当てはめる値が同じ場合は：以降の記述を省略できる
