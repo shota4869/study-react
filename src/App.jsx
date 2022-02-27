a90be7c03b4c2655fe81949f128c11600af89b9f
shota4869 <81454257+shota4869@users.noreply.github.com>
2022-02-27 12:29:04 +0900

    Stateの学習

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert("hello");
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0); //State
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickButton}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
-  console.log(props);
-
   const { color, children } = props; //分割代入
   const contentStyle = {
     color, //プロパティ名と当てはめる値が同じ場合は：以降の記述を省略できる
